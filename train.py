d02a971ca593399889bf5d163be8f238370c0ce4
menaseh kassahun <53655445+menasehk13@users.noreply.github.com>
2023-07-30 12:17:40 +0000

    update .gitignore

--- train.py
+++ train.py
@@ -6,55 +6,58 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 IMAGE_SIZE = (224, 224)
 BATCH_SIZE = 32
 NUM_EPOCHS = 10
-SAVED_MODEL_DIR = "models/saved_model"
 
 def create_model():
-    # Define your model architecture here
     model = tf.keras.Sequential([
-        tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet'),
-        tf.keras.layers.GlobalAveragePooling2D(),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
+            tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet'),
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Dense(64, activation='relu'),  # New hidden layer with ReLU activation
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
     return model
 
-def prepare_dataset(data_dir):
-    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-    train_generator = train_datagen.flow_from_directory(
-        data_dir,
-        target_size=IMAGE_SIZE,
-        batch_size=BATCH_SIZE,
-        class_mode='binary',
-        subset='training'
-    )
-    validation_generator = train_datagen.flow_from_directory(
-        data_dir,
-        target_size=IMAGE_SIZE,
-        batch_size=BATCH_SIZE,
-        class_mode='binary',
-        subset='validation'
-    )
-    return train_generator, validation_generator
+def prepare_dataset():
+    train_dir = './data/train'
+    val_dir = './data/test'
+    train_filenames = os.listdir(train_dir)
+    val_filenames = os.listdir(val_dir)
+
+    # Data preprocessing
+    train_images = []
+    for filename in train_filenames:
+        img = tf.keras.preprocessing.image.load_img(os.path.join(train_dir, filename), target_size=IMAGE_SIZE)
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = img / 255.0  # Normalize to [0, 1]
+        train_images.append(img)
+
+    val_images = []
+    for filename in val_filenames:
+        img = tf.keras.preprocessing.image.load_img(os.path.join(val_dir, filename), target_size=IMAGE_SIZE)
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = img / 255.0  # Normalize to [0, 1]
+        val_images.append(img)
+
+    train_labels = [0] * len(train_images)  # Replace 0 with appropriate labels
+    val_labels = [0] * len(val_images)      # Replace 0 with appropriate labels
+
+    return (tf.convert_to_tensor(train_images), tf.convert_to_tensor(train_labels)), (tf.convert_to_tensor(val_images), tf.convert_to_tensor(val_labels))
 
 def train_model():
-    data_dir = "data"
-    train_generator, validation_generator = prepare_dataset(data_dir)
+    (train_images, train_labels), (val_images, val_labels) = prepare_dataset()
 
     model = create_model()  # Define your model architecture here
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Training the model
     model.fit(
-        train_generator,
-        steps_per_epoch=train_generator.samples // BATCH_SIZE,
+        train_images,
+        train_labels,
+        batch_size=BATCH_SIZE,
         epochs=NUM_EPOCHS,
-        validation_data=validation_generator,
-        validation_steps=validation_generator.samples // BATCH_SIZE
+        validation_data=(val_images, val_labels)
     )
-
-    # Save the trained model
-    # if not os.path.exists(SAVED_MODEL_DIR):
-    #     os.makedirs(SAVED_MODEL_DIR)
-    # model.save(SAVED_MODEL_DIR)
+    
+    model.save('trained_model.h5')
 
 if __name__ == "__main__":
     train_model()
